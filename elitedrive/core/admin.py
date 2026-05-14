c19222fc9de3a0ecab5d8ee8b18944cb48bb162f
katerinajasurkova <d23297@oa-opava.cz>
2026-05-14 11:27:47 +0200

    Delete elitedrive directory

--- elitedrive/core/admin.py
+++
@@ -1,7 +0,0 @@
-from django.contrib import admin
-from .models import Auto, Zamestnanec, Zakaznik, Vypujcka
-
-admin.site.register(Auto)
-admin.site.register(Zamestnanec)
-admin.site.register(Zakaznik)
-admin.site.register(Vypujcka)
